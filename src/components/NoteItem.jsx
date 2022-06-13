c80d1365fa6ea540447fa41ee74e9a6adac2d80b
Sergey Vagaytsev <thevagser@gmail.com>
2022-06-13 16:25:45 +0700

    Научился удалять заметки

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,9 +1,22 @@
+import {useDispatch, useSelector} from 'react-redux';
 
 export default function NoteItem(props) {
+    const dispatch = useDispatch()
+    const allNotes = useSelector(state => state.allNotes)
+
+    const removeNote = () => {
+        dispatch({type: 'REMOVE_NOTE', payload: props.note.id})
+    }
+
     return(
         <div>
             <h3>{props.note.title}</h3>
             <p>{props.note.body}</p>
+            <button
+                onClick={removeNote}
+            >
+                Удалить заметку
+            </button>
         </div>
     )
 }
